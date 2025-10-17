237190c4428bbd61ddc3727165f7ffaa97e25084
SkyllarRisen <20288996+SkyllarRisen@users.noreply.github.com>
2025-10-17 13:09:16 +0200

    Fix style check

--- src/main/java/com/ase/userservice/config/SecurityConfig.java
+++ src/main/java/com/ase/userservice/config/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig {
     http
       .authorizeHttpRequests(authorize -> authorize
         .requestMatchers("/").permitAll()
-          .anyRequest().authenticated()
+        .anyRequest().authenticated()
       )
       .oauth2ResourceServer(oauth2 -> oauth2
         .jwt(Customizer.withDefaults())
